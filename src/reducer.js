c4e8a350532079b4901dc23cb7da0702119c7c90
arielmoatti <github@arielmoatti.com>
2020-11-13 14:45:34 +0100

    PART9 begin

---
+++ src/reducer.js
@@ -0,0 +1,6 @@
+export default function reducer(state = {}, action) {
+    //
+    // place to copy and update the new state....
+
+    return state;
+}
--- src/start.js
+++ src/start.js
@@ -4,14 +4,28 @@ import ReactDOM from "react-dom";
 import Welcome from "./welcome";
 import App from "./app";
 
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import reduxPromise from "redux-promise";
+import { composeWithDevTools } from "redux-devtools-extension";
+import reducer from "./reducer";
+
+const store = createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(reduxPromise))
+);
+
 let elem;
 const userIsLoggedIn = location.pathname != "/welcome";
 
 if (!userIsLoggedIn) {
     elem = <Welcome />;
 } else {
-    elem = <App />;
-    // elem = <img src="/cool_beans_transp.png" className="logo"></img>;
+    elem = (
+        <Provider store={store}>
+            <App />
+        </Provider>
+    );
 }
 
 ReactDOM.render(elem, document.querySelector("main"));
