1c4c1cf1a2252477a93253a7dc4c81004561d0d7
arielmoatti <github@arielmoatti.com>
2021-02-16 12:47:04 +0100

    tiny Heroku fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = (exports.app = express());
 const server = require("http").Server(app);
 // const io = require("socket.io")(server, { origins: "localhost:8080" });
 const io = require("socket.io")(server, {
-    origins: "localhost:8080 https://cool-beans-sn.herokuapp.com/:*",
+    origins: "localhost:8080 https://cool-beans-sn.herokuapp.com:*",
 });
 ////////////////////
 const compression = require("compression");
